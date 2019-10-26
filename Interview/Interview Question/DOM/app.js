326fcbf4f4cc18582ecf1d2464021fd14f510e21
pintaigao <pintaigao@gmail.com>
2019-10-26 16:31:35 -0500

    add 3th source code and React project

---
+++ Interview/Interview Question/DOM/app.js
@@ -0,0 +1,18 @@
+/* 1. Get Body Tag */
+let body = document.body;
+
+/* 2. Get ChildNodes List (Array) */
+let childNode = body.childNodes;
+
+/* 3. append a child to the list */
+body.appendChild("somethingNode");
+
+/* 4. Set the domain */
+document.domain = "some domain";
+
+
+
+
+
+
+
